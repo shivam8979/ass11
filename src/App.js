f890bfda08f4d7fa2670cfe458d9a6bb02b5a8c3
shivam <@shankyadhar.com>
2022-12-15 19:46:18 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Service from './Components/Service';
 import './App.css';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import ThemeContext from './Context';
+
+
 
 function App() {
+  const [isLight, setIsLight]=useState(true);
+  const changeTheme=()=>{
+    setIsLight((prevState)=>{
+      return !prevState
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <ThemeContext.Provider value={{isLight,changeTheme}}>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/service' element={<Service/>}/>
+    </Routes>
+    </ThemeContext.Provider>
+     
+    </BrowserRouter>
   );
 }
 
